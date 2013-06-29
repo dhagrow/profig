bfb4d3990fddd403e50e887a4e0264ef6f943a6c
dhagrow <cymrow@gmail.com>
2013-06-29 00:06:45 -0400

    example tweak
    --HG--
    extra : rebase_source : 201655afa50837f0b02d4dcfa332210415ff5bfe

--- examples/basic.py
+++ examples/basic.py
@@ -1,4 +1,18 @@
+import io
 import config
 
 c = config.Config()
-print(repr(c))
+c.format = config.IniFormat()
+
+c.init('config value', 3, float)
+c.init('a.b.c', 3.4, int)
+c['asdf'] = 234
+
+c._dump()
+
+buf = io.StringIO()
+c.sync(buf)
+print(buf.getvalue())
+
+print(c.section('a.b'))
+print(c)
