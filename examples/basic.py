446cf1eb691002fc797dc7664bae9d02be623ef6
dhagrow <cymrow@gmail.com>
2013-11-23 08:47:35 -0500

    added output for other formats

--- examples/basic.py
+++ examples/basic.py
@@ -2,17 +2,44 @@ import io
 import config
 
 c = config.Config()
-c.format = config.IniFormat()
 
 c.init('config value', 3, float)
 c.init('a.b.c', 3.4, int)
 c['asdf'] = 234
 
+print('Root')
+print(c)
+
+print('\nSection')
+print(c.section('a.b'))
+
+print('\nDump')
 c._dump()
 
+print('\nIniFormat')
 buf = io.StringIO()
+c.format = config.IniFormat()
 c.sync(buf)
 print(buf.getvalue())
+print('len:', len(buf.getvalue()))
 
-print(c.section('a.b'))
-print(c)
+print('\nJsonFormat')
+buf = io.StringIO()
+c.format = config.JsonFormat()
+c.sync(buf)
+print(buf.getvalue())
+print('len:', len(buf.getvalue()))
+
+print('\nConfigFormat')
+buf = io.StringIO()
+c.format = config.ConfigFormat()
+c.sync(buf)
+print(buf.getvalue())
+print('len:', len(buf.getvalue()))
+
+print('\nPickleFormat')
+buf = io.BytesIO()
+c.format = config.PickleFormat()
+c.sync(buf)
+print(repr(buf.getvalue()))
+print('len:', len(buf.getvalue()))
