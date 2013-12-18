cb260e9d440c68945b3d86123116ec01382c164b
dhagrow <cymrow@gmail.com>
2013-12-17 21:33:55 -0500

    updated quickstart

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -4,24 +4,46 @@ cfg = config.Config()
 cfg['server.host'] = '8.8.8.8'
 cfg['server.port'] = 8181
 
+print('--[sync]--')
+print(cfg.asdict())
 cfg.sync('app.cfg')
+print(cfg.asdict())
+
+print('--[app.cfg]--')
+print(open('app.cfg').read())
 
 # Initialization
 
 cfg = config.Config(['app.cfg', 'app2.cfg'])
-cfg.flags['write_unset_values'] = False
 
 cfg.init('server.host', '127.0.0.1')
 cfg.init('server.port', 8080, int)
 cfg.init('server.ssl', False, bool)
 
+print('--[sync]--')
+print(cfg.asdict())
 cfg.sync()
 print(cfg.asdict())
+
+print('--[app.cfg]--')
+print(open('app.cfg').read())
 
 c = config.Config()
 c['server.host'] = '1.1.1.1'
 c['server.ssl'] = True
-c.sync('app2.cfg')
 
+print('--[cfg]--')
+print(cfg.asdict())
+
+print('--[sync]--')
+print(cfg.asdict())
+c.sync('app2.cfg')
+print(cfg.asdict())
+
+print('--[app2.cfg]--')
+print(open('app2.cfg').read())
+
+print('--[sync]--')
+print(cfg.asdict())
 cfg.sync()
 print(cfg.asdict())
