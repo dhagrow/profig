1181e5e71f5f47b3face75f6f6354f3c60585b27
dhagrow <cymrow@gmail.com>
2014-01-02 23:39:02 -0500

    updated docs

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,19 +1,40 @@
 import config
 
+print('Sync')
+print('====')
+
 print('--[Config()]--')
 cfg = config.Config()
 cfg['server.host'] = '8.8.8.8'
 cfg['server.port'] = 8181
+print(cfg.asdict())
 
 print('--[sync(app.cfg)]--')
-print(cfg.asdict())
 cfg.sync('app.cfg')
 print(cfg.asdict())
 
-print('--[app.cfg]--')
+print('>>app.cfg<<')
 print(open('app.cfg').read())
 
+# Unitialized sync
+
+print()
+print('Uninitialized Sync')
+print('==================')
+
+open('app.cfg', 'w+').write("""\
+server.host: 127.0.0.1
+server.port: 8080
+""")
+
+cfg.sync('app.cfg')
+print(cfg.asdict())
+
 # Initialization
+
+print()
+print('Initialization')
+print('==============\n')
 
 print('--[Config(app.cfg, app2.cfg)]--')
 cfg = config.Config('app.cfg', 'app2.cfg')
@@ -27,9 +48,9 @@ print(cfg.asdict())
 cfg.sync()
 print(cfg.asdict())
 
-print('--[app.cfg]--')
+print('>>app.cfg<<')
 print(open('app.cfg').read())
-print('--[app2.cfg]--')
+print('>>app2.cfg<<')
 print()
 
 print('--[Config(app.cfg, app2.cfg)]--')
@@ -42,10 +63,10 @@ print(cfg.asdict())
 cfg.sync('app2.cfg')
 print(cfg.asdict())
 
-print('--[app.cfg]--')
+print('>>app.cfg<<')
 print(open('app.cfg').read())
 
-print('--[app2.cfg]--')
+print('>>app2.cfg<<')
 print(open('app2.cfg').read())
 
 print('--[sync]--')
@@ -60,8 +81,8 @@ print(cfg.asdict())
 cfg.sync()
 print(cfg.asdict())
 
-print('--[app.cfg]--')
+print('>>app.cfg<<')
 print(open('app.cfg').read())
 
-print('--[app2.cfg]--')
+print('>>app2.cfg<<')
 print(open('app2.cfg').read())
