dbb6484188d5cec71d2ef0967c44a4739723bb71
dhagrow <cymrow@gmail.com>
2014-03-06 23:11:05 -0500

    updated docs
    --HG--
    rename : docs/quickstart.txt => docs/usage.txt

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -7,11 +7,11 @@ print('--[Config()]--')
 cfg = config.Config()
 cfg['server.host'] = '8.8.8.8'
 cfg['server.port'] = 8181
-print(cfg.asdict())
+print(cfg.as_dict())
 
 print('--[sync(app.cfg)]--')
 cfg.sync('app.cfg')
-print(cfg.asdict())
+print(cfg.as_dict())
 
 print('>>app.cfg<<')
 print(open('app.cfg').read())
@@ -28,7 +28,7 @@ server.port: 8080
 """)
 
 cfg.sync('app.cfg')
-print(cfg.asdict())
+print(cfg.as_dict())
 
 # Initialization
 
@@ -44,9 +44,9 @@ cfg.init('server.port', 8080, int)
 cfg.init('server.ssl', False, bool)
 
 print('--[sync()]--')
-print(cfg.asdict())
+print(cfg.as_dict())
 cfg.sync()
-print(cfg.asdict())
+print(cfg.as_dict())
 
 print('>>app.cfg<<')
 print(open('app.cfg').read())
@@ -59,9 +59,9 @@ cfg['server.host'] = '1.1.1.1'
 cfg['server.ssl'] = True
 
 print('--[sync(app2.cfg)]--')
-print(cfg.asdict())
+print(cfg.as_dict())
 cfg.sync('app2.cfg')
-print(cfg.asdict())
+print(cfg.as_dict())
 
 print('>>app.cfg<<')
 print(open('app.cfg').read())
@@ -70,16 +70,16 @@ print('>>app2.cfg<<')
 print(open('app2.cfg').read())
 
 print('--[sync]--')
-print(cfg.asdict())
+print(cfg.as_dict())
 cfg.sync()
-print(cfg.asdict())
+print(cfg.as_dict())
 
 cfg['server.host'] = '2.2.2.2'
 
 print('--[sync]--')
-print(cfg.asdict())
+print(cfg.as_dict())
 cfg.sync()
-print(cfg.asdict())
+print(cfg.as_dict())
 
 print('>>app.cfg<<')
 print(open('app.cfg').read())
