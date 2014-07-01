0deb1ca208ced398bd2e75bb97a6fc21ef5731a5
miguel <miguel@immunityinc.com>
2014-06-30 23:54:55 -0400

    bumped version

--- profig.py
+++ profig.py
@@ -23,7 +23,7 @@ import itertools
 import collections
 
 __author__  = 'Miguel Turner'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __license__ = 'MIT'
 
 __all__ = ['Config', 'IniFormat', 'ConfigError', 'Coercer', 'CoerceError']
